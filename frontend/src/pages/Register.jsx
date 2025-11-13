36951886e9c4a2cb4dc92e4fc1c40c78be310732
Developmentexpert121 <developmentexpert121@gmail.com>
2025-11-13 10:19:25 +0530

    design changes

--- frontend/src/pages/Register.jsx
+++ frontend/src/pages/Register.jsx
@@ -12,11 +12,14 @@ export default function Registeryyy() {
   const handleRegister = async (e) => {
     e.preventDefault();
     try {
-      const res = await fetch("https://ai-meeting-assistant-backend-suu9.onrender.com/api/auth/register", {
-        method: "POST",
-        headers: { "Content-Type": "application/json" },
-        body: JSON.stringify(formData),
-      });
+      const res = await fetch(
+        "https://ai-meeting-assistant-backend-suu9.onrender.com/api/auth/register",
+        {
+          method: "POST",
+          headers: { "Content-Type": "application/json" },
+          body: JSON.stringify(formData),
+        }
+      );
 
       let data;
       try {
